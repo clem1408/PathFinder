3c2f822d9a19b5cc14a0a0b8f67b6eb4682b94b5
clem1408 <jourdheuilclement@gmail.com>
2025-01-15 14:47:21 +0100

    Added adjacency lists

--- main.cpp
+++ main.cpp
@@ -6,14 +6,29 @@
 
 using namespace std;
 
-class MyHandler : public osmium::handler::Handler {
+class GraphBuilder : public osmium::handler::Handler {
 public:
+    // Node list (id, (latitude, longitude))
+    unordered_map<int64_t, pair<double, double>> nodes;
+
+    // Adjacency list
+    unordered_map<int64_t, vector<int64_t>> adjacency_list;
+
+    // Function to fill the node list
     void node(const osmium::Node& node) {
-        cout << "Node: " << node.id() << " at " << node.location() << "\n";
+        nodes[node.id()] = {node.location().lat(), node.location().lon()};
     }
 
+    // Function to fill the adjacency list
     void way(const osmium::Way& way) {
-        cout << "Way: " << way.id() << " with " << way.nodes().size() << " nodes\n";
+        const auto& nodes_in_way = way.nodes();
+        for (auto it = nodes_in_way.begin(); it != prev(nodes_in_way.end()); ++it) {
+            int64_t from = it->ref();
+            int64_t to = next(it)->ref();
+            adjacency_list[from].push_back(to);
+            // If bidirectional
+            adjacency_list[to].push_back(from);
+        }
     }
 };
 
@@ -24,9 +39,18 @@ int main(int argc, char* argv[]) {
     }
 
     osmium::io::Reader reader(argv[1], osmium::osm_entity_bits::all);
-    MyHandler handler;
-    osmium::apply(reader, handler);
+    GraphBuilder builder;
+    osmium::apply(reader, builder);
     reader.close();
+
+    // Print of adjacency list
+    /*for (const auto& [node, neighbors] : builder.adjacency_list) {
+        cout << "Node " << node << " connects to: ";
+        for (int64_t neighbor : neighbors) {
+            cout << neighbor << " ";
+        }
+        cout << endl;
+    }*/
 
     return 0;
 }
